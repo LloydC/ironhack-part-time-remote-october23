1df6a05157927870adf4f9ad9215a35842d5ec7c
LloydC <lloydchambrier@gmail.com>
2023-10-03 12:28:57 -0700

    lesson-01 done

---
+++ module1/lesson-01/day-01.js
@@ -0,0 +1,36 @@
+// Variables
+let firstName = "Jon"; // "" '' String
+let lastName = "Chambrier"; // String
+
+const age = 33; // Number
+const email = "lloydchambrier@gmail.com";
+
+let isMarried = false; // Boolean
+let likesCoffee = true; // Boolean
+
+let hobbies = ["Kickboxing", "Hiking", "Meditation"]; // Array
+console.log(hobbies[0])
+console.log(hobbies[2])
+
+let ageRanges = [18, 21, 32, 44]; // Array
+
+const favoriteBook = {
+    bookName: "The Art Of War",
+    author: "Sun Tzu",
+    pages: 98,
+    genres: ["Strategy", "Management", "Leadership"]
+}; // object
+
+let test; // undefined
+// test = "Hey"
+// test = 98
+// test = [true, false, true]
+
+// console.log(lastName.length)// print the firstName variable
+
+if(firstName.length > 4){
+    console.log("First Name has more than 4 characters")
+}
+else{
+    console.log("First Name has less than 4 characters")
+}
