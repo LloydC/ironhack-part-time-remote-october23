50f7101f55f466a117e15ffc3c92aa1030fba3aa
LloydC <lloydchambrier@gmail.com>
2023-10-19 19:34:03 +0200

    lesson-07 code update

--- module1/lesson-07/classes-exercises.js
+++ module1/lesson-07/classes-exercises.js
@@ -4,13 +4,40 @@
 // protein, carbs and fat properties
 // Be sure to test those methods on the instances you created earlier to make sure they work
 
+class Human {
+    constructor(name, gender, age, calories){
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.calories = calories
+    }
+
+    eatFood(food){
+        // const newCaloriesCount = 0;
+        this.calories = this.calories + food.protein + food.carbs + food.fat;
+    }
+}
 // Exercise 2
 // Create a class Food with properties name, protein, carbs, fat
-
-// Add a method printCalories() for classFood that prints out how many calories a specific food provides
 // Carbohydrates provides 4 calories per gram
 // Protein provides 4 calories per gram
 // Fat provides 9 calories per gram
+class Food {
+    constructor(name, protein, carbs, fat){
+        this.name = name;
+        this.protein = protein * 4;
+        this.carbs = carbs * 4;
+        this.fat = fat * 9;
+    }
+}
+
+const firstHuman = new Human("John Doe", 34, "male", 1000);
+const firstFood = new Food( "Alloco", 0, 100, 50);
+firstHuman.eatFood(firstFood)
+console.log('firstHuman', firstHuman)
+console.log('firstFood', firstFood)
+// Add a method printCalories() for classFood that prints out how many calories a specific food provides
+
 // i.e: Eating banana will add 89 calories
 
 // Create instances of each class and print them to the console to confirm they work
