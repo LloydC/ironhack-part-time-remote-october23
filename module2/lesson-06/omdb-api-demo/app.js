9970a96476f06ef6e249c9b0e5bda4c39dddbcf2
LloydC <lloydchambrier@gmail.com>
2023-12-02 12:56:47 +0100

    lesson-06 code update

---
+++ module2/lesson-06/omdb-api-demo/app.js
@@ -0,0 +1,78 @@
+require("dotenv").config(); // Loads the values from .env file to process.env
+const hbs = require("hbs"); // Views
+const path = require("path"); // joins two directories or files
+const axios = require("axios"); // make calls to external APIs
+const morgan = require("morgan"); // logger, show on the terminal which route was recently called
+const express = require("express"); // used to make express server + application
+const app = express();
+const PORT = 8000;
+
+
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+// Middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); // get form data from the client to the server
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/searchResults", (req, res) => {
+  // req.query --> Query String
+  console.log('req.query',req.query)
+  const { movieName, movieDirector } = req.query; // --> const movieName = req.query.movieName
+  const lowercaseMovieName = movieName.toLowerCase();
+
+  axios
+    .get(
+      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_Key}&s=${lowercaseMovieName}&page=1-2`
+    )
+    .then((OMDBResponse) => {
+      const { Search, totalResults } = OMDBResponse.data;
+      console.log(OMDBResponse.data)
+
+      res.render("search-results", { movieName, Search, totalResults });
+    })
+    .catch((error) => console.log(error));
+});
+
+app.get("/movie/:movieId", (req, res) => {
+  console.log(req.params);
+  const { movieId } = req.params;
+
+  axios
+    .get(
+      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_Key}&i=${movieId}`
+    )
+    .then((OMDBMovie) => {
+      console.log(OMDBMovie.data);
+      const movie = OMDBMovie.data;
+
+      res.render("movie", { movie });
+    })
+    .catch((error) => console.log(error));
+});
+
+app.post("/searchResults", (req, res) => {
+  const { movieName } = req.body;
+    console.log('req body', req.body);
+    
+    axios
+    .get(
+      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_Key}&s=${movieName}&page=10`
+    )
+    .then((OMDBResponse) => {
+      const { Search, totalResults } = OMDBResponse.data;
+
+      res.render("search-results", { movieName, Search, totalResults });
+    })
+    .catch((error) => console.log(error));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running ${PORT}`);
+});
---
+++ module2/lesson-06/users-app/app.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose')
+const port = 3000;
+
+const User = require('./models/User.model')
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'hbs');
+
+app.use(express.static(`${__dirname}/public`))
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+mongoose.connect('mongodb://localhost:27017/users-app')
+        .then(() => console.log('Connection to DB successful'))
+        // .then(()=> User.create({username: 'pedrodiaz', email:'pedro@test.com', password:'Test123'}))
+        .catch(err => console.log(err))
+
+app.get("/", (req, res)=>{
+    User.find()
+        .then((allUsers) => {
+            res.render("index", {users: allUsers})
+        })
+        .catch(err => console.log(err))
+})
+
+app.get('/search', (req,res)=>{
+    console.log(req.query)
+    
+    if(req.query.q){
+        User.find({username: req.query.q})
+        .then(foundUsers => {
+            console.log('foundUsers', foundUsers)
+            res.render('search', { users: foundUsers })
+        })
+    }
+    else{
+        res.render('search')
+    }
+    
+})
+
+app.get('/signup', (req,res) => {
+    res.render("signup")
+})
+
+app.post('/signup', (req, res)=>{
+    console.log(req.body)
+    User.create(req.body)
+        .then(newUser => {
+            console.log('newUser', newUser)
+            res.redirect('/')
+            // res.redirect(`/profile/${newUser.username}`)
+        })
+        .catch(err => console.log(err))
+})
+
+app.get('/profile/:username', (req, res)=>{
+    console.log('req.params', req.params)
+    const username = req.params.username;
+
+    User.findOne({username: username})
+        .then( foundUser => {
+            console.log("foundUser", foundUser)
+            res.render("profile", {user: foundUser});
+        })
+})
+
+app.listen(port, ()=> console.log(`Users App is running on port ${port}`))
