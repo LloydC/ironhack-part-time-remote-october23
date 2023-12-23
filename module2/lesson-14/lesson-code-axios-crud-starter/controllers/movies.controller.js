2e3c0695434f51cdcb773fa987a62a019056ee8f
LloydC <lloydchambrier@gmail.com>
2023-12-23 18:53:39 +0100

    lesson-15 code add

--- module2/lesson-14/lesson-code-axios-crud-starter/controllers/movies.controller.js
+++ module2/lesson-14/lesson-code-axios-crud-starter/controllers/movies.controller.js
@@ -14,3 +14,50 @@ const movieListController = (req, res) => {
 const newCharacterController = (req, res) => {
     res.render('pages/new-character-form')
   };
+
+  const createCharacterController = (req, res) => {
+    //   console.log('req.body', req.body);
+    
+      apiService
+        .createCharacter(req.body)
+        .then(() => res.redirect(`/movie-characters/list`))
+        .catch(error => console.log(error));
+    }
+
+    const editCharacterController = (req, res) => {
+        const { id } = req.params;
+        apiService
+        .getOneCharacter(id)
+        .then(response => {
+            res.render('pages/edit-character-form', {character: response.data})
+        })
+        .catch(error => console.log(error));
+    }
+
+    const submitEditCharacterController = (req, res) => {
+        const { id } = req.params;
+        const { name, occupation, weapon } = req.body;
+      
+        apiService
+          .editCharacter(id, { name, occupation, weapon })
+          .then(() => res.redirect(`/movie-characters/list`))
+          .catch(error => console.log(error));
+      }
+
+    const deleteCharacterController =  (req, res) => {
+        const { id } = req.params;
+        
+          apiService
+            .deleteCharacter(id)
+            .then(() => res.redirect(`/movie-characters/list`))
+            .catch(error => console.log(error));
+    }
+
+    module.exports = {
+        movieListController,
+        newCharacterController,
+        createCharacterController,
+        editCharacterController,
+        submitEditCharacterController,
+        deleteCharacterController
+    }
